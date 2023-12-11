5a2f4be6a6ab62c569796d8ef273330f708c18d1
GuaisClement <clement.guais@reseau.eseo.fr>
2023-12-11 16:32:48 +0100

    refacto architecture

---
+++ lib/ui/screens/home.dart
@@ -0,0 +1,51 @@
+// home.dart
+
+import 'package:flutter/material.dart';
+// Importez d'autres dépendances nécessaires
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+  final String title = 'test';
+
+  @override
+  State<Home> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
